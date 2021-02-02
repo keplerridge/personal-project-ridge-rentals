29e8fb755c08f02df69c956b7f12724af8473014
keplerridge <kepler.ridge@gmail.com>
2021-02-02 16:47:20 -0700

    switched auth.js to a hook

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,138 +1,106 @@
 import axios from 'axios';
-import {Component, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import {getUser} from '../../redux/reducer';
 
-class Auth extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            email: '',
-            password: '',
-            verPassword: '',
-            registerView: false,
-            admin: true
+const Auth = props => {
+    const [email, setEmail] = useState(''),
+          [password, setPassword] = useState(''),
+          [verPassword, setVerPassword] = useState(''),
+          [registerView, setRegisterView] = useState(false),
+          [admin, setAdmin] = useState(true);
+
+    useEffect(() => {
+        if(props.user.user_id){
+            props.history.push('/')
         }
-    }
+    }, [props])
 
-    componentDidMount(){
-        if(this.props.user.user_id){
-            this.props.history.push('/')
-            alert('Already logged in, please logout before trying to login in to another account')
-        }
-    }
-
-    handleInput = (event) => {
-        this.setState({[event.target.name]: event.target.value})
-    }
-
-    handleToggle = () => {
-        this.setState({registerView: !this.state.registerView})
-    }
-
-    register = () => {
-        const {email, password, verPassword, admin} = this.state;
+    const register = () => {
 
         if(password && password === verPassword) {
             axios.post('/auth/register', {email, password, admin})
             .then(res => {
-                this.props.getUser(res.data)
-                console.log(this.props.user)
-                this.props.history.push('/')
+                props.getUser(res.data)
+                console.log(props.user)
+                props.history.push('/')
             })
             .catch(err => {
                 console.log(err)
                 alert('Email already in use, please login or select another')
-                this.setState({email: '', password: '', verPassword: ''})
+                setEmail('')
+                setPassword('')
+                setVerPassword('')
             })
         } else {
             alert('Passwords do not match')
         }
     }
 
-    login = () => {
-        const {email, password} = this.state;
+    const login = () => {
 
         axios.post('/auth/login', {email, password})
         .then(res => {
-            this.props.getUser(res.data)
-            this.props.history.push('/profile')
+            props.getUser(res.data)
+            props.history.push('/profile')
         })
         .catch(err => {
             console.log(err)
             alert('Email or password incorrect')
-            this.setState({email: '', password: ''})
+            setEmail('')
+            setPassword('')
         })
     }
 
-    render(){
-        // console.log(this.state)
         return(
             <div>
+                {console.log(registerView)}
                 <section>
                     <h2>Ridge Rentals</h2>
-                    {this.state.registerView 
+                    {registerView 
                     ? (
                         <div>
                             <h3>REGISTER</h3>
                             <input
-                                value={this.state.email}
-                                name='email'
+                                value={email}
                                 placeholder='Email Address'
-                                onChange={e => this.handleInput(e)} />
+                                onChange={e => setEmail(e.target.value)} />
                             <input
-                                value={this.state.password}
-                                name='password'
+                                value={password}
                                 placeholder='Password'
                                 type='password'
-                                onChange={e => this.handleInput(e)} />
+                                onChange={e => setPassword(e.target.value)} />
                             <input
-                                value={this.state.verPassword}
-                                name='verPassword'
+                                value={verPassword}
                                 placeholder='Verify Password'
                                 type='password'
-                                onChange={e => this.handleInput(e)} />
-                            <button onClick={this.register}>Register</button>
+                                onChange={e => setVerPassword(e.target.value)} />
+                            <button onClick={() => register()}>Register</button>
+                            <button onClik={() => setRegisterView(!registerView)}>Back</button>
                         </div>
                     ) : (
                         <div>
                             <h3>Login Here</h3>
                             <input
-                                value={this.state.email}
+                                value={email}
                                 name='email'
                                 placeholder='Email Address'
-                                onChange={e => this.handleInput(e)} />
+                                onChange={e => setEmail(e.target.value)} />
                             <input
-                                value={this.state.password}
+                                value={password}
                                 name='password'
                                 placeholder='Password'
                                 type='password'
-                                onChange={e => this.handleInput(e)} />
-                            <button onClick={this.login}>LOGIN</button>
-                            <p>Need an account? <span onClick={this.handleToggle}>REGISTER HERE</span></p>
+                                onChange={e => setPassword(e.target.value)} />
+                            <button onClick={() => login()}>LOGIN</button>
+                            <p>Need an account? <span onClick={() => setRegisterView(!registerView)}>REGISTER HERE</span></p>
                         </div>
                     )}
                 </section>
             </div>
         )
-    }
 }
 
 const mapStateToProps = reduxState => reduxState;
 
 export default connect(mapStateToProps, {getUser})(Auth);
-
-
-
-// const [email, setEmail] = useState(''),
-//       [password, setPassword] = useState(''),
-//       [verPassword, setVerPassword] = useState(''),
-//       [registerView, setRegisterView] = useState(false),
-//       [admin, setAdmin] = useState(true)
-
-// useEffect(() => {
-//     if(props.user.user_id){
-//         props.history.push('/')
-//         alert('Already logged in, please logout before trying to login in to another account')
-//     }
-// }, [])
