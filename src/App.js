d32574a1ac7a227c164c6cdcf1c19ec78d45b8ef
keplerridge <kepler.ridge@gmail.com>
2021-02-10 09:40:41 -0700

    finished making header responsive on every page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Header from './Components/Header/Header';
-import MobileHeader from './Components/MobileHeader/MobileHeader';
 import Footer from './Components/Footer/Footer';
 import routes from './routes';
 import './App.css';
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MobileHeader />
       {routes}
       <Footer />
     </div>
